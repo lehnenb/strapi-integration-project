c3a7b666c881dfc684ce6f62b1f48f934a1a31b3
Bruno Lehnen <bruno.lehnen@gmail.com>
2019-09-20 15:07:44 -0300

    Add Voluntary form

--- web/src/Voluntary.js
+++ web/src/Voluntary.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { 
+  BrowserRouter as Router, 
+  Route, 
+  Link 
+} from 'react-router-dom';
+
 import {
  Header,
  Menu,
+ Form,
+ Button,
+ Container,
+ TextArea,
 } from 'semantic-ui-react';
 
 function Voluntary() {
@@ -23,7 +32,23 @@ function Voluntary() {
             <Link to='/addHome'>Cadastrar Lar de Idosos</Link>
         </Menu.Item>
       </Menu>
-
+      <Container id='voluntary-form form' >
+        <Form>
+          <Header>Cadastrar Voluntário</Header>
+          <Form.Group unstackable widths={2}>
+            <Form.Input label='Nome:' placeholder='Nome:' />
+            <Form.Input label='Sobrenome:' placeholder='Sobrenome:' />
+          </Form.Group>
+          <Form.Group widths={2}>
+            <Form.Input label='e-mail:' placeholder='e-mail:' />
+            <Form.Input label='Telefone:' placeholder='Telefone:' />
+          </Form.Group>
+          <Form.Group widths={2}>
+            <Form.Field control={TextArea} label="Carta de Apresentação:" placeholder="Digite sua carta de apresentação" />
+          </Form.Group>
+          <Button type='submit'>Enviar</Button>
+      </Form>
+     </Container>
   </div>
   );
 }
