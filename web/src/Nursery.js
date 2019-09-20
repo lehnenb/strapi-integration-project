72907ea60daa9d1c28eb915e444df3cad6964012
Bruno Lehnen <bruno.lehnen@gmail.com>
2019-09-20 15:14:47 -0300

    Add Nursery Form

--- web/src/Nursery.js
+++ web/src/Nursery.js
@@ -4,6 +4,10 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import {
  Header,
  Menu,
+ Form,
+ Button,
+  Container,
+  TextArea,
 } from 'semantic-ui-react';
 
 function Nursery() {
@@ -23,6 +27,23 @@ function Nursery() {
             <Link to='/addHome'>Cadastrar Lar de Idosos</Link>
         </Menu.Item>
       </Menu>
+      <Container id='nursery-form form' >
+        <Form>
+          <Header>Cadastrar Lar</Header>
+          <Form.Group unstackable widths={2}>
+            <Form.Input label='Nome:' placeholder='Nome:' />
+            <Form.Input label='Endereço:' placeholder='Endereço:' />
+          </Form.Group>
+          <Form.Group widths={2}>
+            <Form.Input label='e-mail:' placeholder='e-mail:' />
+            <Form.Input label='Telefone:' placeholder='Telefone:' />
+          </Form.Group>
+          <Form.Group widths={2}>
+            <Form.Field control={TextArea} label="Descrição das Atividades de Voluntariado:" placeholder="Digite a descrição" />
+          </Form.Group>
+          <Button type='submit'>Enviar</Button>
+      </Form>
+     </Container>
   </div>
   );
 }
